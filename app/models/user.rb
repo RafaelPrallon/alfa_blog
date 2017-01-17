5f9aaab51c8201ac2bd3da5a9edf334c5cb89d52
RafaelPrallon <final_rpmx@hotmail.com>
2017-01-17 22:04:45 +0000

    fix length validation on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
-            lenght: { minimum: 3, maximum: 25 }
+            length: { minimum: 3, maximum: 25 }
             
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, lenght: { maximum: 105},
+  validates :email, presence: true, length: { maximum: 105},
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
 
